20274d186f1407dc7b76fa6bdc6669bdb16f2187
DungVPN <dungbgbg12tt@gmail.com>
2025-02-26 17:18:37 +0700

    ss16

--- btap/b5.js
+++ btap/b5.js
@@ -1,4 +1,4 @@
-let a = prompt("nhap so thu 1");
-let b = prompt("nhap so thu 2");
-let sum = parseFloat (a) + parseFloat(b);
+let a = parseInt(prompt("nhap so thu 1"));
+let b = parseInt(prompt("nhap so thu 2"));
+let sum = a + b;
 alert(sum);
